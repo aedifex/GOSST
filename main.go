7c48592449b84f48aa92b5f1e3fab640cdddfcc8
aedifex <aedifex@gmail.com>
2025-01-13 20:22:10 -0800

    Bad rev

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func health(w http.ResponseWriter, r *http.Request) {
 // Used for basic health checks, returning a 200 if the app is up and running.
 func faux(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Faux as in faux pas."))
+	w.Write([]byte("Bad revision?"))
 }
 
 // Return 'get' URI in the body of the response.
